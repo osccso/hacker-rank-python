1ecc7e1f1756591311011b5aa21253954988e92a
osccso <osm428@hotmail.com>
2022-06-20 11:10:14 -0500

    climbinbtheleaderboard, added big data test set

--- climbingtheleaderboardopt.py
+++ climbingtheleaderboardopt.py
@@ -1,4 +1,15 @@
+import time
+from climbingtheleaderboarddata import *
 #optimized using different for loops
+#instead of using append using extend instead
 def climbingLeaderboard(ranked,player):
   for :
+    
+start = time.time()
+
+print(climbingLeaderboard(rankedArray,playerArray))
+end = time.time()
+elapsedTime = end-start
+print('The time elapsed is:',elapsedTime,' in seconds')
+
     
