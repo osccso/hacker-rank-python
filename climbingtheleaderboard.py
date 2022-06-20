49c7925d2b087f47453cecb7ef9119dca7276e6a
osccso <osm428@hotmail.com>
2022-06-19 23:13:55 -0500

    added climbing the leaderboard algorithm, changes made

--- climbingtheleaderboard.py
+++ climbingtheleaderboard.py
@@ -1,14 +1,14 @@
-from audioop import reverse
-
-
 def climbingLeaderboard(ranked,player):
   #check if the ranked leader board is valid
   comparingString = ranked.sort(reverse = True)
+  length = len(ranked)
   if comparingString != ranked :
     return print('String not properly ranked')
   #this is where the positions for the array are going be stored
   ranks = []
+  print('im here')
   for score in player :
+      print(score)
       for index,registered_score in enumerate(ranked) :
         if index == 0 :
           rank = 1
@@ -18,8 +18,14 @@ def climbingLeaderboard(ranked,player):
         if score > registered_score :
           ranks.append(rank)
           break
+        if score < registered_score and length == index+1:
+          rank +=1
+          ranks.append(rank)
+  print(ranks+' - > this is ranks')
   return ranks
-          
+  
+  climbingLeaderboard([100,100,50,40,40,20,10],[5,25,50,120])
+
 
    
     
