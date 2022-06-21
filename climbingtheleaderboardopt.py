a2b836d7751ecbd8fa901e4736e757967cef1a8e
osccso <osm428@hotmail.com>
2022-06-20 23:20:20 -0500

    added optimization to climbingtheleaderboard one test more passed

--- climbingtheleaderboardopt.py
+++ climbingtheleaderboardopt.py
@@ -1,30 +1,29 @@
 import time
-import numpy as np
 from climbingtheleaderboarddataheavy import *
 #optimized using different for loops
 #instead of using append using extend instead
 def climbingLeaderboard(ranked,player):
-  #preallocating data for speed
-  length = len(ranked)
-  lengthPlayer = len(player)
-  ranks = [0]*lengthPlayer
-  rankedMatrix = [0]*length
-  track = float('inf')
-  rank = 0
-  for index,score in enumerate(ranked) :
-    if score < track:
-      rank +=1
-      track = score
-    rankedMatrix[index] = rank
-  #this is where the positions for the array are going be stored
-  for count,score in enumerate(player) :
-      for index,registered_score in enumerate(ranked) :
-        if score >= registered_score :
-          ranks[count]=rankedMatrix[index]
-          break
-        if score < registered_score and length == index+1:
-          ranks[count]=rankedMatrix[index]+1
-  return ranks  
+  #convert list to set
+  #and convert back to list
+  listRanked = list(dict.fromkeys(ranked))
+  #preallocate for speed
+  rankedLength = len(listRanked)
+  playerLength = len(player)
+  ranks = [0]*playerLength
+  count = 0
+  for score in player:
+    countinner = 1
+    if score < listRanked[-1]:
+      ranks[count] = rankedLength+1
+      count += 1
+      continue
+    for data in listRanked:
+      if score >= data:
+        ranks[count] = countinner
+        break
+      countinner += 1
+    count += 1  
+  return ranks
 start = time.time()
 print(climbingLeaderboard(rankedArray,playerArray))
 end = time.time()
