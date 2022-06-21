9bcac5c8bff1c2599f825728a0b6c844cafd6450
osccso <osm428@hotmail.com>
2022-06-20 21:57:42 -0500

    climbingleaderboard data preallocated

--- climbingtheleaderboard.py
+++ climbingtheleaderboard.py
@@ -1,6 +1,6 @@
 import time
 # from climbingtheleaderboarddata import *
-from climbingtheleaderboarddataheavy import *
+from climbingtheleaderboarddata import *
 
 def climbingLeaderboard(ranked,player):
   #check if the ranked leader board is valid
@@ -10,25 +10,27 @@ def climbingLeaderboard(ranked,player):
   if comparingString != ranked :
     return print('String not properly ranked')
   #creates the ranks for the ranked matrix
-  rankedMatrix = []
+  #preallocating data for speed
+  length = len(ranked)
+  lengthPlayer = len(player)
+  ranks = [0]*lengthPlayer
+  rankedMatrix = [0]*length
   track = float('inf')
   rank = 0
-  for score in ranked :
+  for index,score in enumerate(ranked) :
     if score < track:
       rank +=1
       track = score
-    rankedMatrix.append(rank)
+    rankedMatrix[index] = rank
   #this is where the positions for the array are going be stored
-  ranks = []
-  for score in player :
+  for count,score in enumerate(player) :
       for index,registered_score in enumerate(ranked) :
         if score >= registered_score :
-          ranks.append(rankedMatrix[index])
+          ranks[count]=rankedMatrix[index]
           break
         if score < registered_score and length == index+1:
-          ranks.append(rankedMatrix[index]+1)
-      
-  return ranks
+          ranks[count]=rankedMatrix[index]+1
+  return ranks  
 start = time.time()
 print(climbingLeaderboard(rankedArray,playerArray))
 end = time.time()
--- climbingtheleaderboardopt.py
+++ climbingtheleaderboardopt.py
@@ -1,12 +1,31 @@
 import time
-from climbingtheleaderboarddata import *
+import numpy as np
+from climbingtheleaderboarddataheavy import *
 #optimized using different for loops
 #instead of using append using extend instead
 def climbingLeaderboard(ranked,player):
-  for :
-    
+  #preallocating data for speed
+  length = len(ranked)
+  lengthPlayer = len(player)
+  ranks = [0]*lengthPlayer
+  rankedMatrix = [0]*length
+  track = float('inf')
+  rank = 0
+  for index,score in enumerate(ranked) :
+    if score < track:
+      rank +=1
+      track = score
+    rankedMatrix[index] = rank
+  #this is where the positions for the array are going be stored
+  for count,score in enumerate(player) :
+      for index,registered_score in enumerate(ranked) :
+        if score >= registered_score :
+          ranks[count]=rankedMatrix[index]
+          break
+        if score < registered_score and length == index+1:
+          ranks[count]=rankedMatrix[index]+1
+  return ranks  
 start = time.time()
-
 print(climbingLeaderboard(rankedArray,playerArray))
 end = time.time()
 elapsedTime = end-start
