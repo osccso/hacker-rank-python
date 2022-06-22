44b2e583f2f1391e3c91142d0411fc65aec26f93
osccso <osm428@hotmail.com>
2022-06-21 22:56:43 -0500

    added Time Conversion fixed

--- timeconversion.py
+++ timeconversion.py
@@ -1,13 +1,14 @@
 def timeConversion(s):
   ampm = s[-2]+s[-1]
-  hours = int(s[0:2])
+  hours = s[0:2]
   minutes = s[3:5]
   seconds = s[6:8]
   
-  if ampm == 'PM' and hours != 12:
+  if ampm == 'PM' and hours != '12':
+    hours = int(hours)
     hours += 12
     
-  if ampm == 'AM' and hours == 12:
+  if ampm == 'AM' and hours == '12':
     hours = '00'
   
   hours = str(hours)
