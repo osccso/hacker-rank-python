df6bfa217b2fb8fcba422ec8e1c38743eba943e1
osccso <osm428@hotmail.com>
2022-06-22 22:43:57 -0500

    between two sets added

---
+++ betweentwosets.py
@@ -0,0 +1,12 @@
+def getTotalX(a,b):
+  #get the maximun value of a
+  maximum = max(a)
+  #get the minimun value of b
+  minimun = min(b)
+  #find the numbers between the arrays
+  count = 0
+  for num in range(maximum,minimun+1):
+    sumA = sum([num%i for i in a])
+    sumB = sum([i%num for i in b])
+    if sumA == 0 and sumB == 0:
+      count += 1
