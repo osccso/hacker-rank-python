43acae79dfbf32427b5223b007a44249510e0589
osccso <osm428@hotmail.com>
2022-07-15 12:32:38 -0500

    added sequence permutation equation

---
+++ sequenceequation.py
@@ -0,0 +1,12 @@
+def permutationEquation(p):
+    #first create a copy of the array
+    p_copy = p.copy()
+    #sort the items (ascending)
+    p_copy.sort()
+    #for every item sorted find x = p(p(y)) or index(index(x)+1)+1
+    y_list = []
+    for p_element in p_copy:
+        index_of_x =p.index(p_element)+1 
+        y = p.index(index_of_x) + 1
+        y_list.append(y)
+    
